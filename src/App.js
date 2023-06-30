b05820593e07d30ebb5968ef6ef97c5c69fe8620
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-06-30 20:57:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main className="main">
+        <p>15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
