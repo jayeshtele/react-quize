79392f2ab2a516bd29e5fa7740b01ea448c4e571
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-07-01 13:48:20 +0530

    Update Options.js

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Options({ question, answer, dispatch }) {
   return (
     <div className="option">
-      {question.options.map((option,index) => (
+      {question.options.map((option, index) => (
         <button
-          className="btn btn-option"
+          className={`btn btn-option ${}`}
           key={option}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
